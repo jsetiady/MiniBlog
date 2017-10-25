3b882ee88883a8e3733409803966f1692afbc73c
rizky maulana <rizkirachyan@gmail.com>
2017-10-25 14:40:03 +0700

    mysqli bismillah32

--- Db.php
+++ Db.php
@@ -2,13 +2,15 @@
  
 class Db {
 
-    private $servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
-    private $username = "baaafb453f5db3";
-    private $password = "d2e42ccf";
-    private $dbname = "rumaji";
+     
     public $conn;
+
     // get the database connection
-    public function getConnection() {       
+    public function getConnection() {     
+    $servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
+     $username = "baaafb453f5db3";
+     $password = "d2e42ccf";
+     $dbname = "rumaji";  
         $conn = new mysqli($this->$servername, $this->$username, $this->$password, $this->$dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
