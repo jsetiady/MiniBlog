9f32f5c72bbc18a1b9227feb8d1d77444a43710e
rizky maulana <rizkirachyan@gmail.com>
2017-10-25 13:50:10 +0700

    mysqli bismillah21

--- Users.php
+++ Users.php
@@ -37,8 +37,8 @@ class UserHandler{
 		header($this->httpVersion. " ". $statusMessage);		
 		header("Content-Type: application/json; charset=UTF-8");
 				
-		$response = json_encode($users_arr);
-		echo $response;
+		/*$response = json_encode($users_arr);
+		echo $response;*/
 	}
 }
 
