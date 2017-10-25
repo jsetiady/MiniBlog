a3730bfb9f752db4038df5925e9cb21a629d9705
rizky maulana <rizkirachyan@gmail.com>
2017-10-25 13:21:48 +0700

    mysqli bismillah6

--- Db.php
+++ Db.php
@@ -1,22 +1,28 @@
 <?php
  
 class Db {
- 
-    private $host = "ap-cdbr-azure-southeast-b.cloudapp.net";
-    private $db_name = "rumaji";
+    private $servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
     private $username = "baaafb453f5db3";
     private $password = "d2e42ccf";
+    private $dbname = "rumaji";
     public $conn;
+
+    // Create connection
+    
+
+
+
+
  
     // get the database connection
     public function getConnection() {
         $this->$conn->close();
-        $conn = new mysqli($servername, $username, $password, $db_name);
-
-        // Check connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } 
+
         echo "Connected successfully";
  
         return $this->conn;
--- Users.php
+++ Users.php
@@ -1,20 +1,7 @@
 <?php
 
-// require_once("Db.php");
-/*class Users {
- 
-    // database connection and table name
-    private $conn;
-    private $table_name = "users";
-    // object properties
-    public $username;
-    public $name;
- 
-    // constructor with $db as database connection
-    public function __construct($db) {
-        $this->conn = $db;
-    }
-}*/
+require_once("Db.php");
+
 
 
 
@@ -23,20 +10,11 @@ class UserHandler{
 	private $httpVersion = "HTTP/1.1";
 
 	function getUsers() {	
-		$servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
-		$username = "baaafb453f5db3";
-		$password = "d2e42ccf";
-		$dbname = "rumaji";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
+		$db = new Db();
+		$conn = $db->getConnection();
 
 		$sql = "SELECT * FROM users";
-		$result = $conn->query($sql);
+	    $result = $conn->query($sql);
 
 		// users array
 	    $users_arr = array();
@@ -59,25 +37,7 @@ class UserHandler{
 		    $statusMessage = "400 Not Found";
 			$users_arr = array('error' => 'No users found!');
 		}
-		// check if more than 0 record found
-		/*if ($num > 0) {
-		    // retrieve table contents
-		    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		        // extract row
-		        extract($row);
-		        $users_item = array(
-		            "username" => $row['username'],
-		            "name" => $row['name']
-		        );
-		        array_push($users_arr["records"], $users_item);
-		    }
-		    // assign status code
-		    $statusMessage = "200 OK";
-		} else {
-		    $statusMessage = "404 Not Found";
-			$users_arr = array('error' => 'No users found!');
-		}
-*/
+
 		header($this->httpVersion. " ". $statusMessage);		
 		header("Content-Type: application/json; charset=UTF-8");
 				
