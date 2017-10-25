2879a40df61f5bccadbd7c24117d1db81990d021
rizky maulana <rizkirachyan@gmail.com>
2017-10-25 13:28:26 +0700

    mysqli bismillah8

--- Db.php
+++ Db.php
@@ -9,9 +9,8 @@ class Db {
 
     // get the database connection
     public function getConnection() {
-        $this->$conn->close();
         $conn = new mysqli($servername, $username, $password, $dbname);
-        
+
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
