32df5bc13ce1adb86ef6012f6e1687ceffcf58e6
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-26 22:59:16 +0700

    config now stored in environment variable

--- config/db.config.php
+++ config/db.config.php
@@ -1,6 +1,13 @@
 <?php
 
+define("DB_HOST", getenv('DB_HOST'));
+define("DB_USERNAME", getenv('DB_USERNAME'));
+define("DB_PASSWORD", getenv('DB_PASSWORD'));
+define("DB_NAME", getenv('DB_NAME')");
+
+/*
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "root");
 define("DB_NAME", "MiniBlog");
+*/
