d8e14b44e8cbebd2a3f5110365bf312109f230eb
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-26 23:02:01 +0700

    Remove additional quotes

--- config/db.config.php
+++ config/db.config.php
@@ -3,7 +3,7 @@
 define("DB_HOST", getenv('DB_HOST'));
 define("DB_USERNAME", getenv('DB_USERNAME'));
 define("DB_PASSWORD", getenv('DB_PASSWORD'));
-define("DB_NAME", getenv('DB_NAME')");
+define("DB_NAME", getenv('DB_NAME'));
 
 /*
 define("DB_HOST", "localhost");
