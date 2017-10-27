ee399047ce088770bbab0a637d49f144d487aeae
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-27 07:06:51 +0700

    Load jquery datatable

--- src/view/_templates/_footer.php
+++ src/view/_templates/_footer.php
@@ -2,5 +2,7 @@
     <script src="public/js/jquery.min.js"></script>
     <script src="public/js/popper.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/jquery-1.12.4.js"></script>
+    <script src="public/js/jquery.dataTables.min.js"></script>
 
 </html>
