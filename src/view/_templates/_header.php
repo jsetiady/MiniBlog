2a39fac281fae7de255351908ee7fd7f8cd010b4
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-28 15:54:55 +0700

    Update templates

--- src/view/_templates/_header.php
+++ src/view/_templates/_header.php
@@ -2,12 +2,12 @@
     <head>
         <title>Simple Blog - Login Page</title>
         <!-- Icons -->
-        <link href="<?php echo API_URL;?>public/css/font-awesome.min.css" rel="stylesheet">
-        <link href="<?php echo API_URL;?>public/css/simple-line-icons.min.css" rel="stylesheet">
+        <link href="<?php echo API_URL; ?>public/css/font-awesome.min.css" rel="stylesheet">
+        <link href="<?php echo API_URL; ?>public/css/simple-line-icons.min.css" rel="stylesheet">
         
         <!-- Data Tables -->
-        <link href="<?php echo API_URL;?>public/css/jquery.dataTables.min.css" rel="stylesheet">
+        <link href="<?php echo API_URL; ?>public/css/jquery.dataTables.min.css" rel="stylesheet">
 
         <!-- Main styles for this application -->
-        <link href="<?php echo API_URL;?>public/css/style.css" rel="stylesheet">
+        <link href="<?php echo API_URL; ?>public/css/style.css" rel="stylesheet">
     </head>
