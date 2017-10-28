fb773c7523df5576b28cb250f2b08d374bc75164
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-28 21:20:12 +0700

    Local config

--- src/controller/api.config.php
+++ src/controller/api.config.php
@@ -1,5 +1,6 @@
 <?php
 
-define("API_URL", "http://kelompok-4.herokuapp.com/");
+//define("API_URL", "http://kelompok-4.herokuapp.com/");
+define("API_URL", "http://localhost:8888/MiniBlog/");
 
 ?>
