1d920cdc126a6acdeb6bc182b252d9561e2b1aa3
Agate-Levelup-Endraxie <endraxie@yahoo.com>
2017-10-28 07:19:28 +0700

    Revert "Implement Backend to Frontend - Login"
    This reverts commit 4d56a48813bfab29322f554e8be31d00c341723c.

--- FEX/src/view/_templates/_footer.php
+++
@@ -1,8 +0,0 @@
- <!-- Bootstrap and plugins -->
-    <script src="public/js/jquery.min.js"></script>
-    <script src="public/js/popper.min.js"></script>
-    <script src="public/js/bootstrap.min.js"></script>
-    <script src="public/js/jquery-1.12.4.js"></script>
-    <script src="public/js/jquery.dataTables.min.js"></script>
-
-</html>
