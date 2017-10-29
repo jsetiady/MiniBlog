b5e3beca24f05175e2b52dc8b41b8bcf2fe2edbd
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-29 08:05:15 +0700

    Remove reset button

--- src/view/addPost.php
+++ src/view/addPost.php
@@ -78,7 +78,6 @@
                         </div>
                         <div class="card-footer">
                           <button type="submit" id="addPost" class="btn btn-sm btn-primary"><i class="fa fa-dot-circle-o"></i> Submit</button>
-                          <button type="reset" class="btn btn-sm btn-danger"><i class="fa fa-ban"></i> Reset</button>
                         </div>
                       </div>
                     </div>
