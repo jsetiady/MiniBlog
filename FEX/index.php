83a2bf88c691ec96df18c3eee6053d974b8f698a
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-10-31 11:58:16 +0700

    Add sql dumo, Remove unused files

--- FEX/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-include_once("src/controller/Controller.php");
-
-$controller = new Controller();
-
-$module = empty($_GET["module"]) ? "home" : $_GET['module'];
-$controller->invoke($module);
